7549e3674338233b1becef985d0cbda22740137a
Jess <jessicatanhc@gmail.com>
2020-01-30 15:39:23 +0800

    Add user registration to database

--- models/user.js
+++ models/user.js
@@ -5,7 +5,15 @@
  */
 module.exports = (dbPoolInstance) => {
 
+    let registerUser = (name, password, callback)=>{
+    let insertQuery = 'INSERT INTO users (name,password) VALUES($1,$2)';
+    let values = [name, password];
+     dbPoolInstance.query(insertQuery, values, (err)=>{
+        (err)? callback(err) : callback(null);
+     });
+ };
   // `dbPoolInstance` is accessible within this function scope
   return {
+    registerUser
   };
 };
