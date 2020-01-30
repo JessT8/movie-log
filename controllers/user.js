966bdaf6caf427c27d2af9405771cfceb22d07c8
Jess <jessicatanhc@gmail.com>
2020-01-30 15:38:59 +0800

    Add register function

--- controllers/user.js
+++ controllers/user.js
@@ -6,6 +6,19 @@ module.exports = (db) => {
    * Controller logic
    * ===========================================
    */
+   let registerPage = (request,response)=>{
+    response.render("user/register");
+   }
+   //register user
+   let register = (request,response)=>{
+    let username = request.body.username;
+    let password = request.body.password;
+    let hashpassword = sha256(password+SALT);
+    db.users.registerUser(username, hashpassword, (err,result)=>{
+        (err)?response.send(err):response.send("registered");
+    });
+   }
+   //sign user in
    let signin = (request, response)=>{
     response.send("Working");
       }
@@ -15,6 +28,8 @@ module.exports = (db) => {
    * ===========================================
    */
   return {
+    registerPage,
+    register,
     signin
   };
 
