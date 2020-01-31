b4fa7f2d86b30604e70999478f2a41792cfb3712
Jess <jessicatanhc@gmail.com>
2020-01-31 20:51:28 +0800

    Add function to get movie

--- controllers/movie.js
+++ controllers/movie.js
@@ -18,11 +18,12 @@ module.exports = (db) => {
     response.send("About Page");
    }
    let movielist = async (request,response)=>{
-
+    let loggedIn = (isLoggedIn(request))?"true": "false";
      const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
      const movie_response = await fetch(url);
-     const movie_data = await movie_response.json();const data = {movies:movie_data.results};
-     //  let loggedIn = (isLoggedIn(request))?"true": "false";
+     const movie_data = await movie_response.json();
+     const data = {movies:movie_data.results,loggedIn };
+     //
      // const data = {
      //    movies:[
      //    {poster_path: "/pwpGfTImTGifEGgLb3s6LRPd4I6.jpg"},
@@ -36,17 +37,39 @@ module.exports = (db) => {
      //    ],
      //    loggedIn
      // }
-     //https://image.tmdb.org/t/p/w500/
     response.render("movies/movielist", data);
    }
-  /**
+    let getMovie = async (request, response)=>{
+
+     let loggedIn = (isLoggedIn(request))?"true": "false";
+    let movie_id = request.params.id;
+    const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.MOVIE_API_KEY}&append_to_response=videos`;
+    const movie_response = await fetch(url);
+    const movie_data = await movie_response.json();
+        const data = {title:"test",loggedIn };
+// const data = {title:"test",
+//         movies:[movies:movie_data.results
+//         {poster_path: "/pwpGfTImTGifEGgLb3s6LRPd4I6.jpg"},
+//         {poster_path: '/m8eFedsS7vQCZCS8WGp5n1bVD0q.jpg'},
+//         {poster_path: '/wfPHdfofBD5PN96dV96a51B3Ja2.jpg'},
+//         {poster_path: '/3W0v956XxSG5xgm7LB6qu8ExYJ2.jpg'},
+//         {poster_path: '/v6xrz4fr92KY1oNC3HsEvrsvR1n.jpg'},
+//         {poster_path: '/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'},
+//         {poster_path: '/pKKvCaL1TPTVtbI6EeliyND3api.jpg'},
+//         {poster_path: '/pKKvCaL1TPTVtbI6EeliyND3api.jpg'}
+//         ],
+//         loggedIn}
+    response.render("movies/movie",data);
+    }
+   /**
    * ===========================================
    * Export controller functions as a module
    * ===========================================
    */
   return {
     movielist,
-    about
+    about,
+    getMovie
   };
 
 }
