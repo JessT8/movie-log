c691f99e9129430e1f3da4f0be60329350301a97
Jess <jessicatanhc@gmail.com>
2020-02-02 02:39:46 +0800

    Add links for buttons

--- views/watchlist/watchlist.jsx
+++ views/watchlist/watchlist.jsx
@@ -6,14 +6,19 @@ class WatchList extends React.Component {
   render() {
     let watchList = "";
     let movies = this.props.movies;
-    if(movies !== null ){
+    if(movies !== undefined ){
     watchList =  movies.map((movie)=>{
         let img = `https://image.tmdb.org/t/p/w500/${movie.poster}`;
         let movie_link = `/movies/${movie.movieid}`;
+        let favoriteLink = `/watchlist/${movie.movieid}/favorite`;
+        let deleteLink = `/watchlist/${movie.movieid}/delete`;
+        let completeLink = `/watchlist/${movie.movieid}/complete`;
+        let favoriteClass = (movie.favorite)?"icon m-5 clickedHeart":"icon m-5 heart";
+        let completeClass = (movie.completed)?"icon m-5 clickedTick":"icon m-5 tick";
         return (
             <div className="m-3 watchlist">
             <a href={movie_link}>
-            <div className="noanimationposter" style={{backgroundImage: `url(${img})`}}></div></a><div className="d-flex flex-row-reverse center"><div className="icon tick m-5">&#10003;</div><div className="icon heart m-5">&hearts;</div><div className="icon cross m-5">&#10005;</div></div></div>)
+            <div className="noanimationposter" style={{backgroundImage: `url(${img})`}}></div></a><div className="d-flex flex-row-reverse center"><form method="POST" action={completeLink}><button type="submit" className={completeClass}>&#10003;</button></form><form method="POST" action={favoriteLink}><button type="submit" className={favoriteClass}>&hearts;</button></form><form method="POST" action={deleteLink}><button type="submit" className="icon cross m-5">&#10005;</button></form></div></div>);
     });
     }else{
           watchList = (<div>No movie added to watchlist</div>)
@@ -23,9 +28,9 @@ class WatchList extends React.Component {
         <NavLayout loggedIn="true"></NavLayout>
                 <div className="center">
                 <h2>WatchList</h2>
-           <div className="row d-flex">
-        {watchList}
-        </div>
+                <div className="row d-flex">
+                {watchList}
+                </div>
         </div>
         </DefaultLayout>
     );
