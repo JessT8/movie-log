5ff5b1699c1444036bc94bd8a26cc9e383cb109b
Jess <jessicatanhc@gmail.com>
2020-02-01 23:57:35 +0800

    Show movies on watchlist page

--- views/watchlist/watchlist.jsx
+++ views/watchlist/watchlist.jsx
@@ -4,17 +4,29 @@ var NavLayout = require('../layouts/navLayout');
 
 class WatchList extends React.Component {
   render() {
+    let watchList = "";
     let movies = this.props.movies;
- const watchList = movies.map((movie)=>{
-        let img = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-        let movie_link = `/movies/${movie.id}`;
-        return (<a href={movie_link}>
-            <div className="poster m-3" style={{backgroundImage: `url(${img})`}}></div></a>)
+    if(movies !== null ){
+    watchList =  movies.map((movie)=>{
+        let img = `https://image.tmdb.org/t/p/w500/${movie.poster}`;
+        let movie_link = `/movies/${movie.movieid}`;
+        return (
+            <div className="m-3 watchlist">
+            <a href={movie_link}>
+            <div className="noanimationposter" style={{backgroundImage: `url(${img})`}}></div></a><div className="d-flex flex-row-reverse center"><div className="icon tick m-5">&#10003;</div><div className="icon heart m-5">&hearts;</div><div className="icon cross m-5">&#10005;</div></div></div>)
     });
+    }else{
+          watchList = (<div>No movie added to watchlist</div>)
+    }
     return (
         <DefaultLayout title="My WatchList">
-        <NavLayout loggedIn={this.props.loggedIn}></NavLayout>
+        <NavLayout loggedIn="true"></NavLayout>
+                <div className="center">
+                <h2>WatchList</h2>
+           <div className="row d-flex">
         {watchList}
+        </div>
+        </div>
         </DefaultLayout>
     );
   }
