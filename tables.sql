6cb30c063023195c107ea388aa118b0efc81b696
Jess <jessicatanhc@gmail.com>
2020-02-01 13:27:30 +0800

    Modify table

--- tables.sql
+++ tables.sql
@@ -1,19 +1,25 @@
 CREATE TABLE IF NOT EXISTS users(
-  UserId SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   username TEXT,
   password TEXT
 );
 
 CREATE TABLE IF NOT EXISTS follow(
-  FollowId SERIAL PRIMARY KEY,
-  UserId INTEGER,
-  FollowerId INTEGER
+  id SERIAL PRIMARY KEY,
+  userid INTEGER,
+  followerid INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS movie(
-  MovieId SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
+  movieid INTEGER,
   title TEXT,
   plot TEXT,
-  watched BOOLEAN,
-  Recommend BOOLEAN
+  poster TEXT
+);
+
+CREATE TABLE IF NOT EXISTS watchlist(
+  id SERIAL PRIMARY KEY,
+  movieid INTEGER,
+  userid INTEGER
 );
