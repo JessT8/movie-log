74de750ace1a1583a5713348593c87a8daf2a2b6
Jess <jessicatanhc@gmail.com>
2020-02-01 16:42:15 +0800

    Add check queries

--- models/movie.js
+++ models/movie.js
@@ -8,7 +8,7 @@ module.exports = (dbPoolInstance) => {
   // `dbPoolInstance` is accessible within this function scope
   let addMovies = (movie, userid,callback)=>{
     let query = "INSERT INTO movie ( movieid,title,plot,poster) VALUES ($1, $2,$3,$4)";
-    let values = [movie.id, movie.title, movie.plot, movie.poster];
+    let values = [movie.id, movie.title, movie.overview, movie.poster_path];
     dbPoolInstance.query(query, values, (err)=>{
     if(err){
          console.log(err);
@@ -34,20 +34,33 @@ module.exports = (dbPoolInstance) => {
     }
   })
   }
-  let addBookmark = (movie, userid, callback)=>{
+  let addBookmark = (movieid, userid, callback)=>{
     let query = "INSERT INTO watchlist (movieid,userid) VALUES ($1,$2)";
-    let values = [movie.id, userid];
+    let values = [movieid, userid];
     dbPoolInstance.query(query,values, (err)=>{
         if(err){
             console.log(err);
             callback(err);
         }else{
-            console.log(movie.id, userid);
-            checkMovies(movie, userid, callback);
+            callback(null);
+        }
+    })
+  }
+  let checkBookmark = (movieid,userid, callback)=>{
+     let query = "SELECT * FROM watchlist WHERE movieid=$1 AND userid=$2";
+      let values = [movieid, userid];
+    dbPoolInstance.query(query,values, (err, resultQuery)=>{
+        if(err){
+            console.log(err);
+            callback(err, null);
+        }else{
+           (resultQuery.rows.length>0)?callback(null, resultQuery.rows):callback(null, null);
         }
     })
   }
   return {
-    addBookmark
+    checkBookmark,
+    addBookmark,
+    checkMovies
   };
 };
