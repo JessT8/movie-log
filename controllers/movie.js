03df1dae1dabe28f1ee9bb5812aeb9dd5a1920ba
Jess <jessicatanhc@gmail.com>
2020-02-01 16:41:32 +0800

    modify movie controller bookmark

--- controllers/movie.js
+++ controllers/movie.js
@@ -23,57 +23,58 @@ module.exports = (db) => {
      const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
      const movie_response = await fetch(url);
      const movie_data = await movie_response.json();
-     const data = {movies:movie_data.results,loggedIn };
-     //
-     // const data = {
-     //    movies:[
-     //    {poster_path: "/pwpGfTImTGifEGgLb3s6LRPd4I6.jpg"},
-     //    {poster_path: '/m8eFedsS7vQCZCS8WGp5n1bVD0q.jpg'},
-     //    {poster_path: '/wfPHdfofBD5PN96dV96a51B3Ja2.jpg'},
-     //    {poster_path: '/3W0v956XxSG5xgm7LB6qu8ExYJ2.jpg'},
-     //    {poster_path: '/v6xrz4fr92KY1oNC3HsEvrsvR1n.jpg'},
-     //    {poster_path: '/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'},
-     //    {poster_path: '/pKKvCaL1TPTVtbI6EeliyND3api.jpg'},
-     //    {poster_path: '/pKKvCaL1TPTVtbI6EeliyND3api.jpg'}
-     //    ],
-     //    loggedIn
-     // }
+     const data = {
+        movies:movie_data.results,
+        loggedIn};
     response.render("movies/movielist", data);
    }
     let getMovie = async (request, response)=>{
-
-     let loggedIn = (isLoggedIn(request))?"true": "false";
-    let movie_id = request.params.id;
-    const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.MOVIE_API_KEY}&append_to_response=videos`;
-    const movie_response = await fetch(url);
-    const movie_data = await movie_response.json();
-        // const data = {title:"test",loggedIn };
-const data = {title:"test",
-movies:movie_data.results,
-        // movies:[
-        // {poster_path: "/pwpGfTImTGifEGgLb3s6LRPd4I6.jpg"},
-        // {poster_path: '/m8eFedsS7vQCZCS8WGp5n1bVD0q.jpg'},
-        // {poster_path: '/wfPHdfofBD5PN96dV96a51B3Ja2.jpg'},
-        // {poster_path: '/3W0v956XxSG5xgm7LB6qu8ExYJ2.jpg'},
-        // {poster_path: '/v6xrz4fr92KY1oNC3HsEvrsvR1n.jpg'},
-        // {poster_path: '/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'},
-        // {poster_path: '/pKKvCaL1TPTVtbI6EeliyND3api.jpg'},
-        // {poster_path: '/pKKvCaL1TPTVtbI6EeliyND3api.jpg'}
-        // ],
-        loggedIn}
-    response.render("movies/movie",data);
-    }
-    let bookmarkMovie = (request, response) =>{
-        console.log("Controller bookmarkMovie");
-        let movies = {id:"1",title:"fjjs",plot:"plot", poster:"ndfsj"};
-                console.log(request.cookies.user_id);
+        let loggedIn = (isLoggedIn(request))?"true": "false";
+        if(loggedIn=== "true"){
         let user_id = request.cookies.user_id;
-         db.movie.addBookmark(movies, user_id, (error)=>{
+        let movie_id = request.params.id;
+        const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.MOVIE_API_KEY}&append_to_response=videos`;
+        const movie_response = await fetch(url);
+        const movie_data = await movie_response.json();
+        db.movie.checkBookmark(movie_id, user_id, (err,movie)=>{
+        let bookmarked;
+            if(err){
+                console.log(err);
+            }else{
+                bookmarked = (movie === null)? false : true;
+            }
+            const data={movie: movie_data, loggedIn, bookmarked};
+            response.render("movies/movie",data);
+            });
+    }else{
+        response.send("Please sign in to bookmark");
+    }
+    }
+
+    let bookmarkMovie = (request, response) =>{
+        let loggedIn = (isLoggedIn(request))?"true": "false";
+        console.log("Controller bookmarkMovie");
+        let user_id = request.cookies.user_id;
+        let movie_id = request.params.id;
+         db.movie.addBookmark(movie_id, user_id, async (error)=>{
             if(error){
                 response.send(error);
             }else{
-        response.send("bookmarkMovie");}
-    });
+            const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.MOVIE_API_KEY}&append_to_response=videos`;
+        const movie_response = await fetch(url);
+        const movie = await movie_response.json();
+        db.movie.checkMovies(movie, user_id, (err)=>{
+        const data={movie, bookmarked: true, loggedIn}
+        response.render("movies/movie", data);
+        });
+  }
+});
+      }
+
+
+
+  let getWatchlist = (request, response)=>{
+    response.send("watchlist");
   }
    /**
    * ===========================================
@@ -84,7 +85,8 @@ movies:movie_data.results,
     movielist,
     about,
     getMovie,
-    bookmarkMovie
+    bookmarkMovie,
+    getWatchlist
   };
 
 }
