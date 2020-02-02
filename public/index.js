23050a594533f8acef488023a874d777f1d70cb4
Jess <jessicatanhc@gmail.com>
2020-02-02 15:14:26 +0800

    Add active link

--- public/index.js
+++ public/index.js
@@ -20,6 +20,29 @@ const heartClick = (e)=>{
     e.target.classList.remove("icon");
     e.target.classList.add("clickedHeart");
 }
-tick.map(t=>t.addEventListener("click", tickClick));
-heart.map(h=>h.addEventListener("click",heartClick));
-bookmark.addEventListener("click", bookmarkClick);
+// tick.map(t=>t.addEventListener("click", tickClick));
+// heart.map(h=>h.addEventListener("click",heartClick));
+// bookmark.addEventListener("click", bookmarkClick);
+
+switch(document.title){
+case "Completed Movies":
+document.querySelector("#meNav").classList.add("activeNav");
+const complete = document.querySelector("#Completed");
+complete.classList.add("activeNav");
+complete.removeAttribute("href");
+break;
+case "Favorite Movies":
+document.querySelector("#meNav").classList.add("activeNav");
+const favorite = document.querySelector("#Favorite");
+favorite.classList.add("activeNav");
+favorite.removeAttribute("href");
+break;
+case "My WatchList":
+document.querySelector("#meNav").classList.add("activeNav");
+const watchlist = document.querySelector("#Watchlist");
+watchlist.classList.add("activeNav");
+watchlist.removeAttribute("href");
+break;
+default:
+break;
+}
