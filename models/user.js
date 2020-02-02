8e6ecac6fcd8840374642d1d86d4147485937709
Jess <jessicatanhc@gmail.com>
2020-02-02 16:58:32 +0800

    Add query to get users

--- models/user.js
+++ models/user.js
@@ -6,10 +6,10 @@
 module.exports = (dbPoolInstance) => {
 
     let registerUser = (name, password, callback)=>{
-    let insertQuery = 'INSERT INTO users (username,password) VALUES($1,$2)';
+    let insertQuery = 'INSERT INTO users (username,password) VALUES($1,$2) RETURNING *';
     let values = [name, password];
-     dbPoolInstance.query(insertQuery, values, (err)=>{
-        (err)? callback(err) : callback(null);
+     dbPoolInstance.query(insertQuery, values, (err, queryResult)=>{
+        (err)? callback(err, null) : callback(null,queryResult.rows );
      });
  };
  let validateUser = (username,password, callback) => {
@@ -23,9 +23,22 @@ module.exports = (dbPoolInstance) => {
         }
     });
   };
+  let getPeople = (userid, callback)=>{
+    let query = 'SELECT * FROM users WHERE NOT id=$1'
+    let values = [userid];
+
+    dbPoolInstance.query(query, values, (error, queryResult)=>{
+        if(error){
+            callback(error, null);
+        }else{
+            callback(null, queryResult.rows);
+        }
+    })
+  }
   // `dbPoolInstance` is accessible within this function scope
   return {
     registerUser,
-    validateUser
+    validateUser,
+    getPeople
   };
 };
