85ad0632110bb5b42250dbcc20a9faf3507bc3ad
Jess <jessicatanhc@gmail.com>
2020-02-02 15:15:06 +0800

    Add routes for favorite movies

--- routes.js
+++ routes.js
@@ -21,6 +21,7 @@ module.exports = (app, allModels) => {
   app.get('/',movieControllerCallbacks.movielist);
   app.get('/about',movieControllerCallbacks.about);
   app.get('/completed',movieControllerCallbacks.completedMovies);
+  app.get('/favorite',movieControllerCallbacks.favoriteMovies);
   app.post('/watchlist/:id/favorite', movieControllerCallbacks.updateFavorite);
   app.post('/watchlist/:id/complete', movieControllerCallbacks.updateComplete);
   //USERS
