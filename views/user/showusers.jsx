c590ca7d238a01b5e1f07c9eeb165cfd8cd61c72
Jess <jessicatanhc@gmail.com>
2020-02-02 16:57:43 +0800

    Add new page

---
+++ views/user/showusers.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var DefaultLayout = require('../layouts/defaultLayout');
+var NavLayout = require('../layouts/navLayout');
+var MovieLayout = require('../layouts/movieLayout');
+
+class UserList extends React.Component {
+  render() {
+    let users = this.props.users;
+    let userDisplay = "";
+    if(users !== undefined ){
+    userDisplay =  users.map((user)=>{
+        let img = `https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png`;
+        let followLink = `/people/${user.id}`;
+        return (
+            <div className="m-3 profile text-center">
+            <div className="profilePic" style={{backgroundImage: `url(${img})`}}></div><div className=""><h3>{user.username}</h3></div><form method="POST" action={followLink}><button type="submit" className="mb-2 btn btn-primary">Follow</button></form></div>);
+    });
+    }else{
+          watchList = (<div>No movie added to watchlist</div>)
+    }
+    return (
+        <DefaultLayout title="People">
+        <NavLayout loggedIn={this.props.loggedIn}></NavLayout>
+        <div className="center">
+        <h2>People</h2>
+        <div className="row d-flex">
+        {userDisplay}
+        </div>
+        </div>
+        </DefaultLayout>
+    );
+  }
+}
+
+module.exports = UserList;
